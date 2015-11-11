5c8a2f78f307481f7c244b44b3a5c57751d68a90
shan <shanchathuranga@gmail.com>
2015-11-11 20:47:08 +0530

    Project initiated

---
+++ application/views/layouts/main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+	<title>eStore - An Inventory System</title>
+</head>
+<body>
+		
+	<div>
+		<?php $this->load->view($main_content); ?>
+	</div>
+	
+	<br/>
+	<hr/>
+	<footer>
+		<p><center><small>&COPY; Copyright 2015</small></center></p>
+	</footer>
+</body>
+</html>
